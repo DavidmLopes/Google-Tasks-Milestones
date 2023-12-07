3cb98b951b1279828d4e69b6822e58bfa53e142a
David Lopes <david.mlopes15@gmail.com>
2023-12-07 21:42:24 +0000

    Refactor callback

--- app/api/callback/route.tsx
+++ app/api/callback/route.tsx
@@ -1,19 +1,19 @@
-import React from 'react'
+import { type NextRequest } from 'next/server'
+import { cookies } from 'next/headers'
 import jwt from 'jsonwebtoken'
+import { redirect } from 'next/navigation'
 
-export default async function Callback({
-    searchParams,
-}: {
-    searchParams: { code: string }
-}) {
+export async function GET(request: NextRequest) {
+    const code = request.nextUrl.searchParams.get('code') ?? ''
+
     const form = new FormData()
-    form.append('code', searchParams.code)
+    form.append('code', code ? code : '')
     form.append('client_id', process.env.CLIENT_ID ? process.env.CLIENT_ID : '')
     form.append(
         'client_secret',
         process.env.CLIENT_SECRET ? process.env.CLIENT_SECRET : '',
     )
-    form.append('redirect_uri', 'http://localhost:3000/login/callback') // TODO: Maybe not good to hardcode this
+    form.append('redirect_uri', 'http://localhost:3000/api/callback') // TODO: Maybe not good to hardcode this
     form.append('grant_type', 'authorization_code')
 
     const { email, access_token } = await fetch(
@@ -27,7 +27,6 @@ export default async function Callback({
             return response.json()
         })
         .then((data) => {
-            console.log(data)
             const jwt_payload = jwt.decode(data.id_token) as { email: string }
             return {
                 email: jwt_payload.email,
@@ -35,9 +34,8 @@ export default async function Callback({
             }
         })
 
-    return (
-        <div>
-            Callback Hello, {email}, here is your access token = {access_token}
-        </div>
-    )
+    cookies().set('email', email)
+    cookies().set('access_token', access_token)
+
+    redirect('/home')
 }
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,8 +4,8 @@ export default function Login() {
     redirect(
         'https://accounts.google.com/o/oauth2/v2/auth?' +
             `client_id=${process.env.CLIENT_ID}` +
-            '&redirect_uri=http://localhost:3000/login/callback' +
+            '&redirect_uri=http://localhost:3000/api/callback' +
             '&response_type=code' +
-            '&scope=openid%20email',
+            '&scope=openid%20profile%20email',
     )
 }
