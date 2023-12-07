31d1e9bea8ddba13069c1018adfc7cb7d46cc709
David Lopes <david.mlopes15@gmail.com>
2023-12-07 17:16:28 +0000

    Better Home screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
+import Link from 'next/link'
+import ThemeButton from '@components/ThemeButton'
+
 export default function Home() {
-    return <div>Hello World!</div>
+    return (
+        <div className="flex h-screen w-full flex-col items-center justify-center gap-5">
+            <ThemeButton />
+            <div className="text-4xl font-medium">Google Taks Milestones</div>
+            <Link href={'/login'}>
+                <div className="rounded-lg bg-highlights px-6 py-2 text-center font-medium hover:bg-highlights-h">
+                    Login
+                </div>
+            </Link>
+        </div>
+    )
 }
