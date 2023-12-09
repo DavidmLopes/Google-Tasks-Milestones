7242ed6d30c5935fd7d4a457e3f722a60508eb46
David Lopes <david.mlopes15@gmail.com>
2023-12-09 15:15:14 +0000

    Center text in start page

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,9 @@ export default function Home() {
     return (
         <div className="flex h-screen w-full flex-col items-center justify-center gap-5">
             <ThemeButton />
-            <div className="text-4xl font-medium">Google Taks Milestones</div>
+            <div className="text-center text-4xl font-medium">
+                Google Taks Milestones
+            </div>
             <Link href={'/login'}>
                 <div className="rounded-lg bg-highlights px-6 py-2 text-center font-medium hover:bg-highlights-h">
                     Login
